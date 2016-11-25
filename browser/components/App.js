f9ef8aa2a8c410475f05833fd74d5f80bb7997b3
Devon Wesley <dwesley89@gmail.com>
2016-11-25 12:49:50 -0800

    refactored App.js file

--- browser/components/App.js
+++ browser/components/App.js
@@ -2,20 +2,14 @@ import React, {Component} from 'react'
 import ListItem from './ListItem'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      itemList: props.todoList
-    }
-  }
-  listTodoItem(itemList) {
-    return itemList.map((todo, index) =>
+  listTodoItem() {
+    return this.props.todoList.map((todo, index) =>
       <ListItem key={index} item={todo} />
     )
   }
   render() {
     return <div>Hello World
-        <ul>{this.listTodoItem(this.state.itemList)}</ul>
+        <ul>{this.listTodoItem()}</ul>
       </div>
   }
 }
